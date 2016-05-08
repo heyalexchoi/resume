f867caab8ce12adc80f8850590d794d0c30e4d94
heyalexchoi <heyalexchoi@gmail.com>
2016-05-08 15:37:04 -0400

    small edits. build.sh builds 3 themes now.

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 yaml2json alex-choi-resume.yml > alex-choi-resume.json --pretty && 
-hackmyresume BUILD alex-choi-resume.json TO out/resume.all
+hackmyresume BUILD alex-choi-resume.json TO out/compact/resume.all -t compact &&
+hackmyresume BUILD alex-choi-resume.json TO out/modern/resume.all -t modern &&
+hackmyresume BUILD alex-choi-resume.json TO out/positive/resume.all -t positive
